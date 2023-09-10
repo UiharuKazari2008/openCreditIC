8c7b4ede07e6cdacdfec2d84cf9643af68abb6f7
Yukimi Kazari <nomail@internet.net>
2023-09-10 19:35:19 -0400

    Added cooldown and antihogging

--- card-server.js
+++ card-server.js
@@ -76,7 +76,7 @@ app.get(['/dispense/:machine_id/:card', '/withdraw/:machine_id/:card'], (req, re
                             return false;
                         const cooldown_target = dispense_log[dispense_log.length - machine.antihog_trigger].time;
                         const timeDifference = Date.now().valueOf() - cooldown_target;
-                        console.log(`Machine Antihog times : ${machine.antihog_trigger}x${machine.antihog_min}m - ${timeDifference * 60000}m`)
+                        console.log(`Machine Antihog times : ${machine.antihog_trigger}x${machine.antihog_min}m - ${timeDifference / 60000}m`)
                         return timeDifference >= (60000 * machine.antihog_min);
                     }
                     if (history.dispense_log[db.cards[req.params.card].user] && db.antihog_trigger && db.antihog_min) {
@@ -85,7 +85,7 @@ app.get(['/dispense/:machine_id/:card', '/withdraw/:machine_id/:card'], (req, re
                             return false;
                         const cooldown_target = dispense_log[dispense_log.length - db.antihog_trigger].time;
                         const timeDifference = Date.now().valueOf() - cooldown_target;
-                        console.log(`Global Antihog times : ${db.antihog_trigger}x${db.antihog_min}m - ${timeDifference * 60000}m`)
+                        console.log(`Global Antihog times : ${db.antihog_trigger}x${db.antihog_min}m - ${timeDifference / 60000}m`)
                         return timeDifference >= (60000 * db.antihog_min);
                     }
                     if (history.dispense_log[db.cards[req.params.card].user] && db.cooldown_trigger && db.cooldown_min) {
@@ -94,7 +94,7 @@ app.get(['/dispense/:machine_id/:card', '/withdraw/:machine_id/:card'], (req, re
                             return false;
                         const cooldown_target = dispense_log[dispense_log.length - db.cooldown_trigger].time;
                         const timeDifference = Date.now().valueOf() - cooldown_target;
-                        console.log(`Cooldown times : ${db.cooldown_trigger}x${db.cooldown_min}m - ${timeDifference * 60000}m`)
+                        console.log(`Cooldown times : ${db.cooldown_trigger}x${db.cooldown_min}m - ${timeDifference / 60000}m`)
                         return timeDifference >= (60000 * db.cooldown_min);
                     }
                     return false;
