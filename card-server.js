0f237b8c99b7d3fbd08b2f71332c57ee2c4f71db
Yukimi Kazari <nomail@internet.net>
2023-09-10 19:20:10 -0400

    Added cooldown and antihogging

--- card-server.js
+++ card-server.js
@@ -77,6 +77,7 @@ app.get(['/dispense/:machine_id/:card', '/withdraw/:machine_id/:card'], (req, re
                         const last_tap = dispense_log[dispense_log.length - 1].time;
                         const cooldown_target = dispense_log[dispense_log.length - machine.antihog_trigger].time;
                         const timeDifference = last_tap - cooldown_target;
+                        console.log(`Machine Antihog times : ${timeDifference * 60000}m`)
                         return timeDifference <= (60000 * machine.antihog_min);
                     }
                     if (history.dispense_log[db.cards[req.params.card].user] && db.antihog_trigger && db.antihog_min) {
@@ -86,6 +87,7 @@ app.get(['/dispense/:machine_id/:card', '/withdraw/:machine_id/:card'], (req, re
                         const last_tap = dispense_log[dispense_log.length - 1].time;
                         const cooldown_target = dispense_log[dispense_log.length - db.antihog_trigger].time;
                         const timeDifference = last_tap - cooldown_target;
+                        console.log(`Global Antihog times : ${timeDifference * 60000}m`)
                         return timeDifference <= (60000 * db.antihog_min);
                     }
                     if (history.dispense_log[db.cards[req.params.card].user] && db.cooldown_trigger && db.cooldown_min) {
@@ -95,6 +97,7 @@ app.get(['/dispense/:machine_id/:card', '/withdraw/:machine_id/:card'], (req, re
                         const last_tap = dispense_log[dispense_log.length - 1].time;
                         const cooldown_target = dispense_log[dispense_log.length - db.cooldown_trigger].time;
                         const timeDifference = last_tap - cooldown_target;
+                        console.log(`Cooldown times : ${timeDifference * 60000}m`)
                         return timeDifference <= (60000 * db.cooldown_min);
                     }
                     return false;
